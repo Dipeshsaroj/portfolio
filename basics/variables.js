3e5d546c9742d265ad4e170b0edaa855b314c0a3
Dipeshsaroj <177411699+Dipeshsaroj@users.noreply.github.com>
2025-06-14 11:04:26 +0000

    variable

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+console.log("Dipesh");
+const accountId=55;
+let accountEmail="dipeshsaroj";
+var accountPassword="1234";
+accountCity="Allahabad";
+let accountState;
+//accountId=2; not allowed
+accountEmail="dipesh";
+accountPassword="9876";
+accountCity="Kanpur";
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
