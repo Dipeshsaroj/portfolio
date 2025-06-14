51fe07498b664ec4506795f17697a6868f0e601e
Dipeshsaroj <177411699+Dipeshsaroj@users.noreply.github.com>
2025-06-14 12:18:56 +0000

    3conversion

---
+++ basics/3conversion.js
@@ -0,0 +1,9 @@
+let no="33";
+let nan="33abc"
+let inNumber=Number(nan);
+let string="dipesh";
+let stringno=Number(string);
+//console.log(no);
+let emptystring="";
+let emptystringboolean=Boolean(emptystring);
+console.table([typeof no,no,inNumber,nan,stringno,typeof emptystringboolean]);
